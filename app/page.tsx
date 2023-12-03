10aa8804eece2986c74578adae7fb60c0d9633c4
Ran Sayada <ran.sayada@gmail.com>
2023-12-03 13:57:20 +0200

    #3: portfolio hero is finished.

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
+import HeroSection from "./components/HeroSection";
+import { Container } from "postcss";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        Content
+    <main className="flex min-h-screen flex-col bg-[#121212]">
+      <div className="container mx-auto px-12 py-4">
+        <HeroSection />
       </div>
     </main>
   );
