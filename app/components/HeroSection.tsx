0b99341bf6ed7c6704d41a8c6c71c366290d24aa
Ran Sayada <ran.sayada@gmail.com>
2023-12-17 13:32:20 +0200

    #12: minor changes.

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -43,14 +43,23 @@ const HeroSection = () => {
             contact me or check my projects down below.
           </p>
           <div>
-            <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white">
-              Hire Me
-            </button>
-            <button className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white  mt-3">
-              <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
-                Download CV
-              </span>
-            </button>
+            <a href="https://www.linkedin.com/in/ran-sayada/" target="_blank">
+              <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white">
+                Hire Me
+              </button>
+            </a>
+            <a
+              href="./resume/resume-ran-sayada.pdf"
+              download="Ran Sayada - CV"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <button className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white  mt-3">
+                <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
+                  Download CV
+                </span>
+              </button>
+            </a>
           </div>
         </div>
         <div className="col-span-5 place-self-center mt-4 lg:mt-0">
